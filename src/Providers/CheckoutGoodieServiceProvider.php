72931574d99e7b335ebb4eb8322e99e38f36e96e
Mario Kehl <bureau@mariokehl.com>
2022-09-14 08:55:40 +0200

    Refactor JS outside of twig

--- src/Providers/CheckoutGoodieServiceProvider.php
+++ src/Providers/CheckoutGoodieServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace CheckoutGoodie\Providers;
 
+use IO\Helper\ResourceContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 /**
  * Class CheckoutGoodieServiceProvider
@@ -12,9 +15,24 @@ use Plenty\Plugin\ServiceProvider;
 class CheckoutGoodieServiceProvider extends ServiceProvider
 {
     /**
+     * Original plentyShop LTS templates have a priority of 100. Any number less than 100 will indicate a higher priority.
+     */
+    const PRIORITY = 0;
+
+    /**
      * Register the service provider.
      */
     public function register()
     {
+    }
+
+    /**
+     * Include custom functions for the progress bar in the Footer.twig of plentyShop LTS
+     */
+    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.Resources.Import', function (ResourceContainer $container) {
+            $container->addScriptTemplate('CheckoutGoodie::content.Containers.Template.Script');
+        }, self::PRIORITY);
     }
 }
